0a4e280d7de881bbfe59c46507f44920db6d75eb
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2023-03-17 23:43:59 +0100

    Created Javascript exercise folder

---
+++ prac.js
@@ -0,0 +1,10 @@
+let username = "boss"
+let message = "You have threee more exercises"
+
+console.log(message+", "+username+"!")
+
+let name = "Chisom"
+let greeting = "Hi, my name is "
+let myGreeting = greeting+name
+
+console.log(myGreeting)
