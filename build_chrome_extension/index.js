ae4280fc916761cea784b391d05e2c7804bf072a
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2023-05-06 15:49:42 +0100

    Made an update

--- build_chrome_extension/index.js
+++ build_chrome_extension/index.js
@@ -1,4 +1,4 @@
-let myLeads =[]
+let myLeads =["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
@@ -6,3 +6,7 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (i=0, i < myLeads.length, i++) {
+    console.log(myLeads[i])
+}
