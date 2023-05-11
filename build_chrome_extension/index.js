5496838fef9271aa9257f65539aaa138b7d5c367
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2023-05-11 22:49:55 +0100

    Made an update

--- build_chrome_extension/index.js
+++ build_chrome_extension/index.js
@@ -1,8 +1,7 @@
 let myLeads =[]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const ulEl = document.getElementById("ul-econst leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
