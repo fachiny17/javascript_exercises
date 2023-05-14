829ea42f3fcacd32b36cb4fcbb797e53d01769c9
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2023-05-15 00:22:24 +0100

    Made an update

--- build_chrome_extension/index.js
+++ build_chrome_extension/index.js
@@ -11,6 +11,14 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
